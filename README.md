37d19def49ed9479943510460533bf63fe185f1a
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-08-03 09:36:10 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # TTControlInstructionsPalette
-Glyphs.app palette for writing ttfAutohint control instructions.
+
+This is a plugin for the [Glyphs font editor](http://glyphsapp.com/). It adds a palette to the sidebar for writing [ttfAutohint control instructions](https://www.freetype.org/ttfautohint/doc/ttfautohint.html#control-instructions).
+
+
+### Installation
+
+1. Open *Window > Plugin Manager.*
+2. Click the Install button next to the *TT Control Instructions Palette* entry.
+3. Restart Glyphs.
+
+### Usage Instructions
+
+coming soon
+
+### Requirements
+
+The plugin needs Glyphs 2.5 or higher. I assume it will not work in earlier versions.
+
+### License
+
+Copyright 2018 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ TTControlInstructions.glyphsPalette/Contents/Resources/plugin.py
@@ -0,0 +1,84 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Palette Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Palette
+#
+#
+###########################################################################################################
+
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class ControlInstructions (PalettePlugin):
+	dialogName = "com.mekkablue.ControlInstructionsPalette"
+	dialog = objc.IBOutlet()
+	controlInstructionsField = objc.IBOutlet()
+	instanceLabel = objc.IBOutlet()
+	
+	def settings(self):
+		self.name = Glyphs.localize({
+			'en': u'TT Control Instructions',
+			'de': u'TT-Kontrollinstruktionen'
+		})
+		
+		self.min = 200
+		self.max = 1000
+		
+		# Load .nib dialog (without .extension)
+		self.loadNib('IBdialog', __file__)
+	
+	def start(self):
+		# Adding a callback for the 'GSUpdateInterface' event
+		Glyphs.addCallback(self.update, UPDATEINTERFACE)
+	
+	def __del__(self):
+		Glyphs.removeCallback(self.update)
+
+	@objc.IBAction
+	def setInstructions_(self, sender):
+		windowController = self.windowController()
+		if windowController:
+			thisFont = windowController.document().font
+			if thisFont and thisFont.currentTab:
+				currentInstance = thisFont.currentTab.previewInstances
+				if type(currentInstance) != str:
+					currentInstance.customParameters["TTFAutohint control instructions"] = self.controlInstructionsField.stringValue()
+
+	def update( self, sender ):
+		if self.windowController():
+			thisFont = self.windowController().document().font
+			if thisFont and thisFont.currentTab:
+				currentInstance = thisFont.currentTab.previewInstances
+				if currentInstance and type(currentInstance) != str:
+					self.instanceLabel.setStringValue_(currentInstance.name)
+					controlInstructions = currentInstance.customParameters["TTFAutohint control instructions"]
+					if controlInstructions:
+						self.controlInstructionsField.setStringValue_(controlInstructions)
+					else:
+						self.controlInstructionsField.setStringValue_("")
+				else:
+					self.instanceLabel.setStringValue_("Please select an instance")
+					self.controlInstructionsField.setStringValue_("")
+				
+	
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
+	
+	# Temporary Fix
+	# Sort ID for compatibility with v919:
+	_sortID = 0
+	def setSortID_(self, id):
+		try:
+			self._sortID = id
+		except Exception as e:
+			self.logToConsole( "setSortID_: %s" % str(e) )
+	def sortID(self):
+		return self._sortID
+	
